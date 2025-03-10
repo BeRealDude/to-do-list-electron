c92db5eff4710693ba7d1504c558cf26765c05df
Vlad <vlad.nikitin1997@bk.ru>
2025-03-11 01:10:57 +0300

    update init backend

--- backend/preload.js
+++
@@ -1,6 +0,0 @@
-// const { contextBridge, ipcRenderer } = require("electron");
-
-// contextBridge.exposeInMainWorld("electronAPI", {
-//   sendMessage: (message) => ipcRenderer.send("message", message),
-//   onMessage: (callback) => ipcRenderer.on("message", (_, data) => callback(data)),
-// });
